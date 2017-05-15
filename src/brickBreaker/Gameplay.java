a35159d7b5fe945f99debf356d30d548c4dce96e
AlvinnLo <yourfamilyishappy@gmail.com>
2017-05-15 12:11:36 -0700

    051517

--- src/brickBreaker/Gameplay.java
+++ src/brickBreaker/Gameplay.java
@@ -12,21 +12,28 @@ import java.awt.Rectangle;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  * Created by loc8537 on 5/5/2017.
  */
 public class Gameplay extends JPanel implements KeyListener, ActionListener
 {
+    JPanel jp = new JPanel();
+    JLabel jl = new JLabel();
+
     private boolean play =false;
     private int score = 0;
 
-    private int totalBricks = 21;
+    private int totalBricks = 35;
 
     private Timer timer;
     private int delay = 8;
 
     private int playerX = 310;
+    private int playerY = 200;
     //starting position for the slide
 
     private int ballposX = 120;
@@ -38,7 +45,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 
     public Gameplay()
     {
-        map = new MapGenerator(3,7);
+        map = new MapGenerator(5,7);
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
@@ -48,12 +55,16 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 
     public void paint(Graphics g)
     {
-        // background
+        //backgrounds
         g.setColor(Color.black);
         g.fillRect(1,1, 692, 592);
-
-        //drawing map
         map.draw((Graphics2D)g);
+
+        //billy herrington image
+        //jl.setIcon(new ImageIcon("billy.jpg"));
+        //jp.add(jl);
+        //add(jp);
+        //validate();
 
         // borders
         g.setColor(Color.yellow);
@@ -68,15 +79,29 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 
         //directions
         g.setColor(Color.white);
-        g.setFont(new Font("serif", Font.BOLD, 22));
-        g.drawString("Controls: A for left and D for Right", 15, 30);
+        g.setFont(new Font("serif", Font.BOLD, 15));
+        g.drawString("Controls: A for left and D for Right For Blue Paddle", 15, 25);
+        g.setFont(new Font("serif", Font.BOLD, 15));
+        g.drawString("                S for left and W for Right For Green Paddle", 15, 40);
+        g.setFont(new Font("serif", Font.BOLD, 15));
+        g.drawString("Paddles don't work when they intersect.", 15, 500);
+        g.setFont(new Font("serif", Font.BOLD, 15));
+        g.drawString("Or tap Q to win immediately...", 255, 150);
 
         // the paddle
-        g.setColor(Color.green);
+        g.setColor(Color.blue);
         g.fillRect(playerX, 550, 100, 8);
 
-        // the ball
+        // paddle2
+        g.setColor(Color.green);
+        g.fillRect(playerY, 550, 100, 8);
+
+        // the ball 1
         g.setColor(Color.yellow);
+        g.fillOval(ballposX, ballposY, 20, 20);
+
+        // the ball 2
+        g.setColor(Color.pink);
         g.fillOval(ballposX, ballposY, 20, 20);
 
         if(totalBricks <= 0){
@@ -85,19 +110,29 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
             ballYdir = 0;
             g.setColor(Color.RED);
             g.setFont(new Font("serif", Font.BOLD, 30));
-            g.drawString("You Won: ", 260, 300);
+            g.drawString("You won: ", 260, 300);
 
             g.setFont(new Font("serif", Font.BOLD, 20));
             g.drawString("Press Enter to Restart ", 230, 350);
         }
+
+
 
         if(ballposY > 570){
             play = false;
             ballXdir = 0;
             ballYdir = 0;
             g.setColor(Color.RED);
-            g.setFont(new Font("serif", Font.BOLD, 30));
-            g.drawString("You like embarrasing me huh?", 140, 300);
+
+            if(score<60){
+                g.setFont(new Font("serif", Font.BOLD, 50));
+                g.drawString("Trash !!!!", 240, 300);
+            }
+
+            if(score>60){
+                g.setFont(new Font("serif", Font.BOLD, 30));
+                g.drawString("You like embarrassing me huh?", 140, 300);
+            }
 
             g.setFont(new Font("serif", Font.BOLD, 30));
             g.drawString("Score:" + score, 270, 350);
@@ -107,17 +142,22 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
 
         }
 
+
         g.dispose();
 
     }
 
-    @Override
     public void actionPerformed(ActionEvent e)
     {
         timer.start();
         if(play)
         {
             if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8)))
+            {
+                ballYdir = -ballYdir;
+            }
+
+            if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerY, 550, 100, 8)))
             {
                 ballYdir = -ballYdir;
             }
@@ -173,12 +213,12 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
         repaint();
     }
 
-    @Override
+
     public void keyTyped(KeyEvent e) {}
-    @Override
+
     public void keyReleased(KeyEvent e) {}
 
-    @Override
+
     public void keyPressed(KeyEvent e)
     {
         if(e.getKeyCode() == KeyEvent.VK_D)
@@ -204,6 +244,36 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
                 moveLeft();
             }
         }
+
+        if(e.getKeyCode() == KeyEvent.VK_W)
+        {
+            if(playerY >= 600)
+            {
+                playerY = 600;
+            }
+            else
+            {
+                moveUp();
+            }
+        }
+
+        if(e.getKeyCode() == KeyEvent.VK_S)
+        {
+            if(playerY < 10)
+            {
+                playerY = 10;
+            }
+            else
+            {
+                moveDown();
+            }
+        }
+
+        if(e.getKeyCode() == KeyEvent.VK_Q)
+        {
+            destroyer();
+        }
+
         if(e.getKeyCode() == KeyEvent.VK_ENTER){
             if(!play){
                 play = true;
@@ -213,12 +283,25 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
                 ballYdir = -2;
                 playerX = 310;
                 score = 0;
-                totalBricks = 21;
-                map = new MapGenerator(3, 7);
+                totalBricks = 35;
+                map = new MapGenerator(5, 7);
 
                 repaint();
             }
         }
+    }
+    public void destroyer()
+    {
+        play = true;
+        for(int i = 0; i<map.map.length; i++) {
+            for (int j = 0; j < map.map[0].length; j++) {
+                map.setBrickValue(0, i, j);
+                totalBricks -= 1;
+                score = 175;
+            }
+        }
+
+
     }
 
     public void moveRight()
@@ -231,6 +314,18 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener
     {
         play = true;
         playerX -= 20;
+    }
+
+    public void moveUp()
+    {
+        play = true;
+        playerY += 20;
+    }
+
+    public void moveDown()
+    {
+        play = true;
+        playerY -= 20;
     }
 
 
--- src/brickBreaker/Main.java
+++ src/brickBreaker/Main.java
@@ -12,7 +12,7 @@ public class Main
         JFrame obj = new JFrame();
         Gameplay gameplay = new Gameplay();
         obj.setBounds(10, 10, 700, 600);
-        obj.setTitle("Bochi Yan :D");
+        obj.setTitle("No Bochi No Life");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
