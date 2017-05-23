72724b43caa18199070f5740e1c6075afb37558c
BoyanChi <1009365729@qq.com>
2017-05-23 14:11:34 -0700

    1

--- src/brickBreaker/Main.java
+++ src/brickBreaker/Main.java
@@ -11,12 +11,12 @@ public class Main
     {
         JFrame obj = new JFrame();
         Gameplay gameplay = new Gameplay();
-        Soundeffect soundeffect = new Soundeffect();
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Alvin Lo's (dark) fantasy ");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         obj.add(gameplay);
+        Soundeffect soundeffect = new Soundeffect();
     }
 }
